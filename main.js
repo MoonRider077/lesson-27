191bbb716a18e6c8787202ba5e723d63f27e2e70
Moon Rider <itachi0777@mail.ru>
2023-10-11 16:45:44 +0500

    Bankai Hakkano Togame

---
+++ main.js
@@ -0,0 +1,145 @@
+// For 1 //
+
+// let k = parseInt(prompt("Enter the number: "));
+
+// for (let n = 1; n <= k; n++) {
+//     alert(n);
+// }
+
+// if (k <= 0) {
+//     alert("Enter the valid number");
+// }
+
+
+
+// For 2 //
+
+// let a = parseInt(prompt("Enter the number: "));
+// let b = parseInt(prompt("Enter the number: "));
+
+// for (let a = 1; a <= b; a++) {
+//     alert(a, b);
+// }
+
+
+
+// For 3 //
+
+// let a = parseInt(prompt("Enter the number: "));
+// let b = parseInt(prompt("Enter the number: "));
+
+// if (a < b) {
+//   for (let i = b - 1; i > a; i--) {
+//     alert(i);
+//   }
+// } else {
+//   alert("B should be greater");
+// }
+
+
+
+// For 4 //
+
+// const k = parseInt(prompt("Enter the number: ")) 
+
+// for (let w = 1; w <= 10; w++) {
+//     const p = k * w;
+//     alert(`${w} kg price: ${p}`);
+// }
+
+
+
+// For 5 //
+
+// const k = parseFloat(prompt("Enter the number: "));
+
+// for (let w = 0.1; w <= 1; w += 0.1) {
+//     const p = k * w;
+//     alert(`${w.toFixed(1)} kg price: ${p.toFixed(2)}`);
+// }
+
+
+
+// For 6 //
+
+// const k = parseFloat(prompt("Enter the number: "));
+
+// for (let w = 1.2; w <= 2; w += 0.1) {
+//     const p = k * w;
+//     alert(`${w.toFixed(1)} kg price: ${p.toFixed(2)}`);
+// }
+
+
+
+// For 7 //
+
+// let a = parseInt(prompt("Enter the number: "));
+// let b = parseInt(prompt("Enter the number: "));
+
+// for (let a = 1; a <= b; a++) {
+//     alert(a, b);
+// }
+
+
+
+// For 8 //
+
+// let a = parseInt(prompt("Enter the number: "));
+// let b = parseInt(prompt("Enter the number: "));
+
+// for (let a = 1; a <= b; a+= 2) {
+//     alert(a, b);
+// }
+
+
+
+// For 9 //
+
+// let a = parseInt(prompt("Enter the number: "));
+// let b = parseInt(prompt("Enter the number: "));
+
+// for (let a = 1; a <= b; a*= 2) {
+//     alert(a, b);
+// }
+
+
+
+// For 10 //
+
+// let n = parseInt(prompt("Enter the number: "));
+// let a = 0;
+
+// for (let a = 0; a < n; a = a + (1/n)) {
+//     alert(a , n);
+// }
+
+
+
+// For 12 //
+
+// const N = parseInt(prompt("Enter the value of N:"));
+
+// let result = 1;
+
+// for (let i = 1; i <= N; i++) {
+//     result *= (1 + (i * 0.1));
+// }
+
+// alert(`The product of numbers from 1.1 to ${1.1 + 0.1 * N} is: ${result.toFixed(2)}`);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
